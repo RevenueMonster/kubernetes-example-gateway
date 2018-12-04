d4a7ed6d6e25fef5bf3378ceab0dfd590ac10c05
yussuf <charu.yussuf@gmail.com>
2018-12-05 00:13:33 +0800

    remove pods description

--- main.go
+++ main.go
@@ -1,26 +1,43 @@
 package main
 
 import (
+	"io/ioutil"
+	"net/http"
 	"os"
-	"time"
 
 	"github.com/labstack/echo"
 )
-
-var podDescription = ""
-
-func init() {
-	podDescription = "Pod started at " + time.Now().UTC().Format("15:04:05")
-}
 
 func main() {
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
+
 		return c.JSON(200, map[string]interface{}{
-			"hostname":    os.Getenv("HOSTNAME"),
-			"message":     os.Getenv("SYSTEM_NAME"),
-			"description": podDescription,
+			"hostname": os.Getenv("HOSTNAME"),
+			"message":  os.Getenv("SYSTEM_NAME"),
+		})
+	})
+
+	e.GET("/helloworld", func(c echo.Context) error {
+
+		response, err := http.Get("http://rm-service")
+		if err != nil {
+			return c.JSON(500, map[string]interface{}{
+				"error": err.Error(),
+			})
+		}
+
+		defer response.Body.Close()
+		contents, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			return c.JSON(500, map[string]interface{}{
+				"error": err.Error(),
+			})
+		}
+
+		return c.JSON(200, map[string]interface{}{
+			"message": string(contents),
 		})
 	})
 
